705a2a9cb69cca4a773dbdce40745fb7cc477e99
jordan <jordan@jordans-MacBook-Air.local>
2020-02-20 17:28:33 +0700

    basic component layout for the app page

--- youtubeclone/src/App.js
+++ youtubeclone/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import youtube from './api/youtube';
+
+
 
 class App extends React.Component{
     render(){
         return(
-            <h1>Youtube App</h1>
+            <Grid justify="center" container spacing={16}>
+                <Grid item x5={12}>
+                    <Grid container spacing={16}>
+                        <Grid item x5={12}>
+                            {/* SEARCH BAR */}
+                        </Grid>
+                        <Grid item x5={8}>
+                        {/* VIDEO DETAIL */}
+                        </Grid>
+                        <Grid item x5={4}>
+                            {/* VIDEO LIST */}
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+
+            </Grid>
         )
     }
 }

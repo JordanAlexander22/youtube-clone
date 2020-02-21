3b58eeef425f61916132cb5a806bb388a1e1af63
jordan <jordan@jordans-MacBook-Air.local>
2020-02-21 14:14:40 +0700

    video detail component functionality

--- youtubeclone/src/components/videoDetail.js
+++ youtubeclone/src/components/videoDetail.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import {Paper, Typography} from '@material-ui/core'
+import React from "react";
+import { Paper, Typography } from "@material-ui/core";
 
-const VideoDetail = ({video}) => {
-    if(!video) return <div>Loading..</div>
-    console.log(video);
-    const videosrc= `https://www.youtube.com/embed/${video.id.videoId}`
+const VideoDetail = ({ video }) => {
+  if (!video) return <div>Loading..</div>;
+  console.log(video);
+  const videosrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
-    return (
-        <React.Fragment>
-            <Paper elevation= {6} style={{height: '70%'}}>
-                <iframe frameBorder='0' height= '100%' width='100%' title="video player" src={videosrc}/>
-            </Paper>
-            <Paper elevation = {6} style={{padding: '1rem'}}>
-                <Typography> </Typography>
-                <Typography> </Typography>
-                <Typography> </Typography>
-            </Paper>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Paper elevation={6} style={{ height: "70%" }}>
+        <iframe
+          frameBorder="0"
+          height="100%"
+          width="100%"
+          title="video player"
+          src={videosrc}
+        />
+      </Paper>
+      <Paper elevation={6} style={{ padding: "1rem" }}>
+        <Typography variant="h4">
+          {video.snippet.title}- {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle1">
+          {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle2">
+            {video.snippet.description} 
+        </Typography>
+      </Paper>
+    </React.Fragment>
+  );
 };
 
 export default VideoDetail;
